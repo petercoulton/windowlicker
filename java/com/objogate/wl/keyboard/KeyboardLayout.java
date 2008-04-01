3109a5d821bcc03fcd8a8c2ad2e1850690901751
npomfret <npomfret@ac37430e-0f47-0410-9073-f3f9481b7c33>
2008-04-01 20:18:14 +0000

    logging to try and fix build
    git-svn-id: http://windowlicker.googlecode.com/svn/trunk@126 ac37430e-0f47-0410-9073-f3f9481b7c33

--- java/com/objogate/wl/keyboard/KeyboardLayout.java
+++ java/com/objogate/wl/keyboard/KeyboardLayout.java
@@ -51,6 +51,7 @@ public class KeyboardLayout implements SystemProperties {
 
     public Gesture gestureForTyping(char ch) {
         KeyStroke keyStroke = keyStrokeFor(ch);
+        System.out.println("keyStroke = " + keyStroke + " " + ch);
 
         return withModifierMask(keyStroke.getModifiers(), typeKey(keyStroke.getKeyCode()));
     }
