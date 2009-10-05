e7613e87fbb29f39e614d5814a9ca8413a8aaaa6
nat.pryce <nat.pryce@ac37430e-0f47-0410-9073-f3f9481b7c33>
2009-10-05 15:35:15 +0000

    addressing issue 3 (cope with new exceptions thrown by webdriver)
    git-svn-id: http://windowlicker.googlecode.com/svn/trunk@245 ac37430e-0f47-0410-9073-f3f9481b7c33

--- src/web/main/com/objogate/wl/web/ElementProbe.java
+++ src/web/main/com/objogate/wl/web/ElementProbe.java
@@ -2,19 +2,19 @@ package com.objogate.wl.web;
 
 import org.hamcrest.Description;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
-
 import com.objogate.wl.Probe;
 
 public abstract class ElementProbe implements Probe {
     private final AsyncElementDriver driver;
-    
+
     private WebElement foundElement = null;
-    
+
     public ElementProbe(AsyncElementDriver driver) {
         this.driver = driver;
     }
-    
+
     public void probe() {
         foundElement = null;
         try {
@@ -24,25 +24,27 @@ public abstract class ElementProbe implements Probe {
         catch (NoSuchElementException e) {
             // try next time
         }
+        catch (StaleElementReferenceException e) {
+            // try next time
+        }
     }
-    
+
     public boolean isSatisfied() {
         return foundElement != null;
     }
-    
+
     public void describeTo(Description description) {
         description.appendDescriptionOf(driver);
     }
-    
+
     public boolean describeFailureTo(Description description) {
         if (foundElement == null) {
             description.appendText("did not find matching element");
             return true;
-        } 
-        else {
+        } else {
             return false;
         }
     }
-    
+
     protected abstract void probe(WebElement element);
 }
