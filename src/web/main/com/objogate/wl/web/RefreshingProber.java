d34ac9826cd41e9f261e20e369e198be7516b0a5
nat.pryce <nat.pryce@ac37430e-0f47-0410-9073-f3f9481b7c33>
2010-07-14 17:00:52 +0000

    nat - better refreshing prober
    git-svn-id: http://windowlicker.googlecode.com/svn/trunk@262 ac37430e-0f47-0410-9073-f3f9481b7c33

--- src/web/main/com/objogate/wl/web/RefreshingProber.java
+++ src/web/main/com/objogate/wl/web/RefreshingProber.java
@@ -1,20 +1,43 @@
 package com.objogate.wl.web;
 
+import org.hamcrest.Description;
 import org.openqa.selenium.WebDriver;
-
-import com.objogate.wl.PollingProber;
 import com.objogate.wl.Probe;
+import com.objogate.wl.Prober;
 
-public class RefreshingProber extends PollingProber {
+/**
+ * A Prober that wraps another Prober to refresh WebDriver every time it performs a probe.
+ *
+ * This is useful for testing read-only web pages, the contents of which are updated asynchronously
+ * w.r.t. the test.
+ */
+public class RefreshingProber implements Prober {
     private final WebDriver webDriver;
+    private final Prober prober;
 
-    public RefreshingProber(WebDriver webDriver) {
+    public RefreshingProber(WebDriver webDriver, Prober prober) {
         this.webDriver = webDriver;
+        this.prober = prober;
     }
 
-    @Override
-	protected void runProbe(Probe probe) {
-    	webDriver.navigate().refresh();
-    	probe.probe();
+    public void check(final Probe realProbe) {
+        prober.check(new Probe() {
+            public void probe() {
+                webDriver.navigate().refresh();
+                realProbe.probe();
+            }
+
+            public boolean isSatisfied() {
+                return realProbe.isSatisfied();
+            }
+
+            public void describeTo(Description description) {
+                realProbe.describeTo(description);
+            }
+
+            public void describeFailureTo(Description description) {
+                realProbe.describeFailureTo(description);
+            }
+        });
     }
 }
