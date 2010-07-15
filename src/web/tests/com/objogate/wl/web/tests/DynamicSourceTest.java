11b9e312e59794d39cf9f46719d409a652fd5d4a
nat.pryce <nat.pryce@ac37430e-0f47-0410-9073-f3f9481b7c33>
2010-07-15 09:25:37 +0000

    nat - test of the refreshing prober
    git-svn-id: http://windowlicker.googlecode.com/svn/trunk@264 ac37430e-0f47-0410-9073-f3f9481b7c33

--- src/web/tests/com/objogate/wl/web/tests/DynamicSourceTest.java
+++ src/web/tests/com/objogate/wl/web/tests/DynamicSourceTest.java
@@ -1,34 +1,83 @@
 package com.objogate.wl.web.tests;
 
-import static org.junit.Assert.fail;
-
 import java.io.File;
 import java.io.IOException;
-
-import org.junit.Before;
-import org.junit.Ignore;
+import java.io.InputStream;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import org.apache.commons.io.IOUtils;
+import org.junit.After;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
+import com.objogate.exception.Defect;
 import com.objogate.wl.Prober;
+import com.objogate.wl.UnsynchronizedProber;
 import com.objogate.wl.web.AsyncWebDriver;
 import com.objogate.wl.web.RefreshingProber;
 
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.apache.commons.io.FileUtils.writeStringToFile;
+import static org.hamcrest.Matchers.equalTo;
+
 public class DynamicSourceTest {
     WebDriver webDriver = new FirefoxDriver();
-    Prober prober = new RefreshingProber(webDriver);
-    AsyncWebDriver driver = new AsyncWebDriver(prober, webDriver);
+    Prober prober = new UnsynchronizedProber();
+    AsyncWebDriver driver = new AsyncWebDriver(new RefreshingProber(webDriver, prober), webDriver);
 
-    @Before
-    public void startCreatingTmp() throws IOException {
-        File tmpFile = File.createTempFile(getClass().getSimpleName(), ".tmp");
-        File committedFile = File.createTempFile(getClass().getSimpleName(), ".html");
-    }
-    
+    ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+
     @Test
-    @Ignore("not finished yet")
-    public void refreshesThePageUntilContentsMatches() {
-        fail("not finished yet");
+    public void refreshesThePageUntilContentsMatches() throws IOException {
+        final File htmlFile = File.createTempFile(getClass().getSimpleName(), ".html");
+        final String template = loadResource("dynamic-source.html");
+
+        writeStringToFileAtomically(htmlFile, template.replace("REPLACE-ME", "Waiting"));
+
+        driver.navigate().to(htmlFile.toURI().toURL());
+        driver.element(By.id("dynamic")).assertText(equalTo("Waiting"));
+
+        scheduler.schedule(new Runnable() {
+            public void run() {
+                writeStringToFileAtomically(htmlFile, template.replace("REPLACE-ME", "Done"));
+            }
+        }, 100, MILLISECONDS);
+
+        driver.element(By.id("dynamic")).assertText(equalTo("Done"));
+    }
+
+    private void writeStringToFileAtomically(File htmlFile, String contents) {
+        try {
+            writeStringToFile(htmlFile, contents);
+        } catch (IOException e) {
+            throw new Defect("failed to write HTML file", e);
+        }
+    }
+
+    private String loadResource(String name) throws IOException {
+        InputStream in = getClass().getResourceAsStream(name);
+        if (in == null) {
+            throw new Defect("could not load resource " + name);
+        }
+
+        try {
+            return IOUtils.toString(in);
+        } finally {
+            in.close();
+        }
+    }
+
+
+    @After
+    public void stopTimer() throws InterruptedException {
+        scheduler.shutdown();
+        scheduler.awaitTermination(1, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void closeBrowser() {
+        driver.quit();
     }
 }
